b54dd078ad67fa70a2252ca6eed4295c7369106e
Akshay Kirane <akshaykirane@Akshays-MacBook-Pro.local>
2018-02-03 10:50:06 -0500

    Initial Commit

---
+++ LV1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LV1
+//
+//  Created by Akshay Kirane on 2/3/18.
+//  Copyright © 2018 Akshay Kirane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
